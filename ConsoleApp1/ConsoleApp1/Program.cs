71c9a1fa8e7305eef909c0b44eac4dbea9547da8
Giedrasdangus <126971598+Giedrasdangus@users.noreply.github.com>
2023-03-07 00:31:37 +0200

    nusikopijavau programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> Bendradarbiai = new List<string>(4);
+
+            Bendradarbiai.Add("Andrius");
+            Bendradarbiai.Add("Edvinas");
+            Bendradarbiai.Add( "Justas");
+            Bendradarbiai.Add ("Darius");
+  
+            foreachfff(string i in Bendradarbiai)
+            {
+                Console.WriteLine("bendradarbis {0}", i);
+            }
+        }
+    }
+}
